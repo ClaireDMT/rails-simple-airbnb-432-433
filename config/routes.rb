c80648cbaba4fbd5f108e0b5c396676aae3d9f25
claire demont <claire.edith.demont@gmail.com>
2020-08-25 14:21:01 +0200

    first push

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  root to: "flats#index"
+  resources :flats, except: :index
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
